b6a0e46c4638032696d507645c03f0f8ce41bed9
Ben Bradshaw <ben@crimson.net.nz>
2016-07-01 11:01:12 +1200

    Formatting changes from Arduino IDE

--- whare_ora_gateway/whare_ora_gateway.ino
+++ whare_ora_gateway/whare_ora_gateway.ino
@@ -1,42 +1,42 @@
 /**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************
- *
- * DESCRIPTION
- * The ArduinoGateway prints data received from sensors on the serial link. 
- * The gateway accepts input on seral which will be sent out on radio network.
- *
- * The GW code is designed for Arduino Nano 328p / 16MHz
- *
- * Wire connections (OPTIONAL):
- * - Inclusion button should be connected between digital pin 3 and GND  
- * - RX/TX/ERR leds need to be connected between +5V (anode) and digital pin 6/5/4 with resistor 270-330R in a series
- *
- * LEDs (OPTIONAL):
- * - To use the feature, uncomment WITH_LEDS_BLINKING in MyConfig.h
- * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
- * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
- * - ERR (red) - fast blink on error during transmission error or recieve crc error 
- * 
- */
+   The MySensors Arduino library handles the wireless radio link and protocol
+   between your home built sensors/actuators and HA controller of choice.
+   The sensors forms a self healing radio network with optional repeaters. Each
+   repeater and gateway builds a routing tables in EEPROM which keeps track of the
+   network topology allowing messages to be routed to nodes.
 
-#define NO_PORTB_PINCHANGES  
+   Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+   Copyright (C) 2013-2015 Sensnology AB
+   Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+
+   Documentation: http://www.mysensors.org
+   Support Forum: http://forum.mysensors.org
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   version 2 as published by the Free Software Foundation.
+
+ *******************************
+
+   DESCRIPTION
+   The ArduinoGateway prints data received from sensors on the serial link.
+   The gateway accepts input on seral which will be sent out on radio network.
+
+   The GW code is designed for Arduino Nano 328p / 16MHz
+
+   Wire connections (OPTIONAL):
+   - Inclusion button should be connected between digital pin 3 and GND
+   - RX/TX/ERR leds need to be connected between +5V (anode) and digital pin 6/5/4 with resistor 270-330R in a series
+
+   LEDs (OPTIONAL):
+   - To use the feature, uncomment WITH_LEDS_BLINKING in MyConfig.h
+   - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
+   - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
+   - ERR (red) - fast blink on error during transmission error or recieve crc error
+
+*/
+
+#define NO_PORTB_PINCHANGES
 
 #include <MySigningNone.h>
 #include <MyTransportRFM69.h>
@@ -45,9 +45,9 @@
 #include <MySigningAtsha204Soft.h>
 #include <MySigningAtsha204.h>
 
-#include <SPI.h>  
-#include <MyParserSerial.h>  
-#include <MySensor.h>  
+#include <SPI.h>
+#include <MyParserSerial.h>
+#include <MySensor.h>
 #include <stdarg.h>
 #include <PinChangeInt.h>
 #include "GatewayUtil.h"
@@ -58,7 +58,7 @@
 #define RADIO_RX_LED_PIN    11  // Receive led pin
 //#define RADIO_TX_LED_PIN    5  // the PCB, on board LED
 
-// NRFRF24L01 radio driver (set low transmit power by default) 
+// NRFRF24L01 radio driver (set low transmit power by default)
 MyTransportNRF24 transport(RF24_CE_PIN, RF24_CS_PIN, RF24_PA_LEVEL_GW);
 //MyTransportRFM69 transport;
 
@@ -67,7 +67,7 @@ MyTransportNRF24 transport(RF24_CE_PIN, RF24_CS_PIN, RF24_PA_LEVEL_GW);
 //MySigningAtsha204Soft signer;
 //MySigningAtsha204 signer;
 
-// Hardware profile 
+// Hardware profile
 MyHwATMega328 hw;
 
 // Construct MySensors library (signer needed if MY_SIGNING_FEATURE is turned on in MyConfig.h)
@@ -92,9 +92,8 @@ void output(const char *fmt, ... ) {
    Serial.print(serialBuffer);
 }
 
-  
-void setup()  
-{ 
+void setup()
+{
   gw.begin(incomingMessage, 0, true, 0);
 
   setupGateway(INCLUSION_MODE_PIN, INCLUSION_MODE_TIME, output);
@@ -117,7 +116,7 @@ void loop()
     // A command wass issued from serial interface
     // We will now try to send it to the actuator
     parseAndSend(gw, inputString);
-    commandComplete = false;  
+    commandComplete = false;
     inputPos = 0;
   }
 }
@@ -125,17 +124,17 @@ void loop()
 
 /*
   SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
- */
+  hardware serial RX.  This routine is run between each
+  time loop() runs, so using delay inside loop can delay
+  response.  Multiple bytes of data may be available.
+*/
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read(); 
+    char inChar = (char)Serial.read();
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
-    if (inputPos<MAX_RECEIVE_LENGTH-1 && !commandComplete) { 
+    if (inputPos < MAX_RECEIVE_LENGTH - 1 && !commandComplete) {
       if (inChar == '\n') {
         inputString[inputPos] = 0;
         commandComplete = true;
@@ -145,8 +144,8 @@ void serialEvent() {
         inputPos++;
       }
     } else {
-       // Incoming message too long. Throw away 
-        inputPos = 0;
+      // Incoming message too long. Throw away
+      inputPos = 0;
     }
   }
 }
